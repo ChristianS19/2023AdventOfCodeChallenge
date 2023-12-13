c99e4096208bb9a56e140b5ee4bc3794f72c910b
Chris S <shackletonchristian+git@gmail.com>
2023-12-13 13:14:30 +0100

    Initial Commit, Description

---
+++ Day01_Trebuchet.py
@@ -0,0 +1,16 @@
+"""
+Calibration numbvaluesers have been incorrectly stored for targeting the trebuchet. On each line, the calibration value can 
+be found by combining the first digit and the last digit (in that order) of the characters to form a single two-digit number. 
+For example:
+
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142
+
+Consider your entire calibration document. What is the sum of all of the calibration values?
+
+Mixed up calibration values can be found at "https://adventofcode.com/2023/day/1/input"
+"""
